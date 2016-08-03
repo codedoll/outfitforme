efce4cf176fa8c66b6abbdfff059f1c500721b03
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-08-03 10:42:11 -0400

    adding files

--- public/app.js
+++ public/app.js
@@ -1,107 +1,83 @@
-var app = angular.module('OutfitForMe', ['ngRoute']);
+var app = angular.module('OutfitForMe', ['ngRoute', 'angularMoment', 'ngRoute','ngSanitize']);
+
+// app.run(function(amMoment) {
+//     amMoment.changeLocale('de');
+// });
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
+    $locationProvider.html5Mode({ enabled: true,   requireBase: false
+ });
 
     $routeProvider
 
-    // .when('/', {
-    //     templateUrl: 'partial/home_page.html',
-    //     controller: 'MangaController',
-    //     controllerAs: 'mctrl'
-    // })
+    .when('/', {
+        templateUrl: 'partial/home.html',
+        controller: 'MainController',
+        controllerAs: 'mctrl'
+    })
 
-    // .when('/admin', {  
-    //     templateUrl: '/admin.html',  
-    //     controller: 'AdminController',
-    //     controllerAs: 'actrl'
-    // })  
+    .when('/ga', {
+        templateUrl: 'google.html',
+        controller: 'MainController',
+        controllerAs: 'mctrl'
+    })
 }])
 
-// console.log('app.js loaded');
-app.controller('MainController', ['$http', '$route', function($http, $route) {
-    // var self = this;
-    // var usernameLogged = "GUEST";
-    // this.sayHello = function() {
-    //     alert("saHello()")
-    // }
-
-    // //registration popup
-    // this.registerModal = function() {
-    //         // $scope.manga = manga;
-
-    //         ngDialog.open({
-    //             template: '/partial/user_register_partial.html',
-    //             // className: 'ngdialog-theme-plain',
-    //             // controller: 'MangaController',
-    //             scope: $scope
-    //         });
-    //     }
-    //     //
 
 
-    // // login function
-    // // the function for submit button on login form gets the user data from DB
-    // this.loginForm = function(loginform) {
+
+app.controller('MainController', ['$http', '$scope', '$routeParams', '$route', function($http, $scope, $routeParams, $route) {
+
+    var self = this;
+    moment.tz.add([
+        'America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0',
+        'America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0'
+    ]);
+
+    // this.analytics = function() {
+    //     // console.log('hi from analytics');
     //     $http({
-    //         url: '/user/login',
-    //          method: 'POST',
-    //          data: loginform
+    //         url: '/analytics',
+    //         method: 'GET'
     //     }).then(function(response) {
     //         console.log(response.data);
-    //         if (response.data.user != "INVALID") {
-    //             self.usernameLogged = response.data.sessionID;
-    //             //flip partials to the user menu partials
-    //         }
-
-    //         $route.reload();
-
     //     })
     // };
 
+    // this.analytics();
 
-    this.analytics = function() {
-        console.log('hi from analytics');
+
+
+    // this.analytics2 = function() {
+    //     // console.log('hi from analytics');
+    //     $http({
+    //         url: '/analytics2',
+    //         method: 'GET'
+    //     }).then(function(tokens) {
+    //         // console.log(tokens.data);
+    //         $scope.token = tokens.data;
+    //     })
+    // };
+
+    // this.analytics2();
+
+
+    this.forecast = function(userLocation) {
+        // console.log(userLocation)
         $http({
-            url: '/analytics',
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response.data);
+            url: '/forecast/' + userLocation.zipcode,
+            method: 'GET',
+            // data: userLocation
+        }).then(function(data) {
+            console.log(data);
+            self.darksky = data.data;
+            self.sky = data.data.hourly.data
+            console.log(self.sky);
+
         })
     };
 
-    this.analytics();
-
-
-        this.analytics2 = function() {
-        console.log('hi from analytics');
-        $http({
-            url: '/analytics2',
-            method: 'GET'
-        }).then(function(tokens) {
-            console.log(tokens.data);
-        })
-    };
-
-    this.analytics2();
-
-
-    // var report = new gapi.analytics.report.Data({
-    //     query: {
-    //         ids: 'ga:XXXX',
-    //         metrics: 'ga:sessions',
-    //         dimensions: 'ga:city'
-    //     }
-    // });
-
-    // report.on('success', function(response) {
-    //     console.log(response);
-    // });
-
-    // report.execute();
+    
 
 
 }]); // end MainController
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+require('dotenv').config();
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var moment = require('moment');
+var moment = require('moment-timezone');
 var bcrypt = require('bcrypt');
 var path = require('path');
+var zipcodes = require('zipcodes');
+var moment = require('moment');
+var _ = require('lodash');
 
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
-var CLIENT_ID = "721951980911-emhegsm8nem373s60qriuvnmbufcpkug.apps.googleusercontent.com";
-var CLIENT_SECRET = "XmiwoVyzihPNnTVYb2Ny3oHr";
+
+// Include envfile 
+var envfile = require('envfile')
+var sourcePath = 'file.env'
+var sourceString = "a=1\nb:2"
+var sourceObject = { a: 1, b: 2 }
+
+
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.ANALYTICS_SECRET;
 var REDIRECT_URL = "http://localhost:4000/oauth2callback";
 
 var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
@@ -35,9 +48,8 @@ var googleapis = require('googleapis'),
     JWT = googleapis.auth.JWT,
     analytics = googleapis.analytics('v3');
 
-var SERVICE_ACCOUNT_EMAIL = 'outfitformeanalytics@appspot.gserviceaccount.com';
+var SERVICE_ACCOUNT_EMAIL = process.env.SERVICE_ACCOUNT_EMAIL;
 var SERVICE_ACCOUNT_KEY_FILE = __dirname + '/client_secret.json';
-
 
 var authClient = new JWT(
     SERVICE_ACCOUNT_EMAIL,
@@ -49,11 +61,9 @@ var port = process.env.PORT || 4000
     // var MONGODBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/forumProj'
 var app = express();
 
-
 // mongoose.connect(MONGODBURI);
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(methodOverride('_method'));
-
 
 app.use(session({
     secret: "beagle",
@@ -61,63 +71,168 @@ app.use(session({
     saveUninitialized: false
 }));
 
-
 app.use(express.static('public'));
-
-
 app.use(bodyParser.json());
 
 
 
+// app.get('/analytics', function(req, res) {
+//     // res.sendFile(path.resolve(__dirname + '/public/index.html'));
+
+//     authClient.authorize(function(err, tokens) {
+//         if (err) {
+//             // console.log(err);
+//             return;
+//         }
+
+//         analytics.data.ga.get({
+//             auth: authClient,
+//             'ids': 'ga:119753868',
+//             'start-date': '30daysAgo',
+//             'end-date': 'yesterday',
+//             'metrics': 'ga:visits'
+//         }, function(err, data) {
+//             // console.log(err);
+//             // console.log(data);
+//             res.send(data)
+//         });
+
+
+//         analytics.data.ga.get({
+//             auth: authClient,
+//             'ids': 'ga:119753868',
+//             'start-date': '30daysAgo',
+//             'end-date': 'yesterday',
+//             'metrics': 'ga:avgSessionDuration'
+//         }, function(err, data) {
+//             // console.log(err);
+//             // console.log(data);
+//             res.send(data)
+//         });
+
+
+//     });
+
+// })
+
 app.get('/analytics', function(req, res) {
-    // res.sendFile(path.resolve(__dirname + '/public/index.html'));
+    // ar express = require(
+    var wordpressPosts = require('wordpress-posts');
+    var wordpressSiteUrl = "http://codedoll.com/";
+    var posts = 4;
+
+    wordpressPosts.get(wordpressSiteUrl, posts, function(err, data) {
+        // console.log(data);
+    });
+
+    var google = require('googleapis');
+    var OAuth2 = google.auth.OAuth2;
+
+    var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
     authClient.authorize(function(err, tokens) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        analytics.data.ga.get({
-            auth: authClient,
-            'ids': 'ga:119753868',
-            'start-date': '2015-01-19',
-            'end-date': '2017-01-19',
-            'metrics': 'ga:visits'
-        }, function(err, data) {
-            // console.log(err);
-            // console.log(data);
-            res.send(data)
-        });
-
+        console.log(tokens.access_token);
+        res.send(tokens.access_token)
 
     });
 
+
+
+    // authClient.authorize(function(err, tokens) {
+    //     if (err) {
+    //         // console.log(err);
+    //         return;
+    //     }
+
+    //     analytics.data.ga.get({
+    //         auth: authClient,
+    //         'ids': 'ga:119753868',
+    //         'start-date': '30daysAgo',
+    //         'end-date': 'yesterday',
+    //         'metrics': 'ga:visits'
+    //     }, function(err, data) {
+    //         // console.log(err);
+    //         // console.log(data);
+    //         res.send(data)
+    //     });
+
+
+    //     analytics.data.ga.get({
+    //         auth: authClient,
+    //         'ids': 'ga:119753868',
+    //         'start-date': '30daysAgo',
+    //         'end-date': 'yesterday',
+    //         'metrics': 'ga:avgSessionDuration'
+    //     }, function(err, data2) {
+    //         // console.log(err);
+    //         // console.log(data);
+    //         res.send(data2)
+    //     });
+
+
+    // });
+
+
+
+});
+
+app.get('/forecast', function(req, res) {
+    var ForecastIO = require('forecast-io')
+    var forecast = new ForecastIO(process.env.DARKSKY)
+
+    forecast
+        .latitude('37.8267') //required: latitude, string.
+        .longitude('-122.423') // required: longitude, string.
+        .time('2016-07-30') // optional: date, string 'YYYY-MM-DD'.
+        .units('auto') // optional: units, string, refer to API documentation.
+        .language('en') // optional: language, string, refer to API documentation.
+        .exclude('minutely,daily') // optional: exclude, string, refer to API documentation.
+        .extendHourly(true) // optional: extend, boolean, refer to API documentation.
+        .get() // execute your get request.
+        .then(function(data) { // handle your success response.
+            res.send(data)
+        })
+        .catch(err => { // handle your error response.
+            console.log(err)
+        })
+
+});
+
+
+app.get('/forecast/:id', function(req, res) {
+    res.header("Content-Type", "application/json");
+
+    var location = zipcodes.lookup(req.params.id);
+
+    var ForecastIO = require('forecast-io')
+    var forecast = new ForecastIO(process.env.DARKSKY)
+
+    forecast
+        .latitude(location.latitude.toString()) //required: latitude, string.
+        .longitude(location.longitude.toString()) // required: longitude, string.
+        .time(moment().format('YYYY-MM-DD')) // optional: date, string 'YYYY-MM-DD'.
+        .units('auto') // optional: units, string, refer to API documentation.
+        .language('en') // optional: language, string, refer to API documentation.
+        .exclude('minutely,daily') // optional: exclude, string, refer to API documentation.
+        .extendHourly(true) // optional: extend, boolean, refer to API documentation.
+        .get() // execute your get request.
+        .then(function(data) { // handle your success response.
+
+            var parseData = JSON.parse(data); //Parses the results from string to an obj
+            res.send((_.merge(location, parseData)))
+        })
+        .catch(err => { // handle your error response.
+            console.log(err)
+        })
+
+});
+
+
+mongoose.connection.once('open', function() {
+    console.log('connected to mongod');
 })
 
 
-app.get('/analytics2', function(req, res) {
-            var google = require('googleapis');
-            var OAuth2 = google.auth.OAuth2;
-
-            var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-
-            authClient.authorize(function(err, tokens) {
-                    console.log(tokens);
-  					res.send(tokens)
-                    })
-
-                
-
-            });
-
-
-
-        mongoose.connection.once('open', function() {
-            console.log('connected to mongod');
-        })
-
-
-        app.listen(port, function() {
-            console.log('listening');
-        })
+app.listen(port, function() {
+    console.log('listening');
+})
