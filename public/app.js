c19c32779b9d084638df7364d15f8d86487cedd2
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-08-03 17:45:49 -0400

    skyicon

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
-var app = angular.module('OutfitForMe', ['ngRoute', 'angularMoment', 'ngRoute','ngSanitize']);
+var app = angular.module('OutfitForMe', ['ngRoute', 'angularMoment', 'ngRoute', 'ngSanitize']);
 
 // app.run(function(amMoment) {
 //     amMoment.changeLocale('de');
 // });
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode({ enabled: true,   requireBase: false
- });
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
 
     $routeProvider.when('/', {
         templateUrl: 'partial/home.html',
@@ -27,6 +29,9 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 app.controller('MainController', ['$http', '$scope', '$routeParams', '$route', function($http, $scope, $routeParams, $route) {
 
     var self = this;
+
+
+
     moment.tz.add([
         'America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0',
         'America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0'
@@ -59,7 +64,7 @@ app.controller('MainController', ['$http', '$scope', '$routeParams', '$route', f
 
     // this.analytics2();
 
-
+    var currentIcon;
     this.forecast = function(userLocation) {
         // console.log(userLocation)
         $http({
@@ -67,15 +72,24 @@ app.controller('MainController', ['$http', '$scope', '$routeParams', '$route', f
             method: 'GET',
             // data: userLocation
         }).then(function(data) {
-            console.log(data);
+
             self.darksky = data.data;
+            
             self.sky = data.data.hourly.data
-            console.log(self.sky);
+
+            var skycons = new Skycons({ "color": "orange" });
+
+            var currentIcon = data.data.currently.icon.toUpperCase();
+            
+            var currentIcon2 = currentIcon.replace(/[_-]/g, "_");
+
+            skycons.add("icon1", Skycons[currentIcon2]);
+
+            skycons.play();
 
         })
     };
 
-    
 
 
 }]); // end MainController
